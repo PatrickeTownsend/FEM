61351067f597bccab84fe7316526e617315aaf60
PatrickeTownsend <p.townsend.2019@alumnos.urjc.es>
2023-11-03 12:33:35 +0100

    error and plotting functions added

--- functions.py
+++ functions.py
@@ -37,15 +37,15 @@ def discretize(Lx,ne,NodesPerElem):
     if Linear:
         nodesNum = ne+1
         nodes = np.linspace(0,Lx,nodesNum)
-        elements = np.zeros((nodesNum-1,2),int)
-        for i in range(0,nodesNum-1):
+        elements = np.zeros((ne,2),int)
+        for i in range(0,ne):
             elements[i,:]+=[i,i+1]
     elif cuadratic:
         nodesNum = 2*ne+1
         nodes = np.linspace(0,Lx,nodesNum)
-        elements = np.zeros((nodesNum-1,2),int)
-        for i in range(0,nodesNum-1):
-            elements[i,:]+=[i,i+1]
+        elements = np.zeros((ne,3),int)
+        for i in range(0,ne):
+            elements[i,:]=[2*i,2*i+1,2*i+2]
     return nodes,elements
 
 
@@ -79,10 +79,17 @@ def plot_mesh_1D(NodeList, ElemList):
 
     plt.xlim([-0.25, NodeList[:].max() + 0.25])
     plt.ylim([-0.25, 0.25])
+    plt.title("Discretización del dominio")
 
     return
 
-def interpolation(N_matrix,nodes,elements):
+def interpolation(N_matrix,nodes,elements,NodesPerElem):
+    if NodesPerElem == 2:
+        Linear = True
+        cuadratic = False
+    else:
+        cuadratic = True
+        Linear = False
     NumElement = np.shape(elements)[0]
     N_matrix = np.transpose(N_matrix)
     x_global = np.zeros((NumElement,np.shape(N_matrix)[0]))
@@ -94,8 +101,14 @@ def interpolation(N_matrix,nodes,elements):
         x2 = nodes[end]
         u1 = Desp_function(x1)
         u2 = Desp_function(x2)
-        x_local = np.array([[x1],[x2]])
-        u_local = np.array([[u1],[u2]])
+        if Linear:
+           x_local = np.array([[x1],[x2]])
+           u_local = np.array([[u1],[u2]])
+        elif cuadratic:
+           x3 = nodes[elements[i][2]]
+           u3 = Desp_function(x3)
+           x_local = np.array([[x1],[x2],[x3]])
+           u_local = np.array([[u1],[u2],[u3]])
         x = np.dot(N_matrix,x_local)
         u = np.dot(N_matrix,u_local)
         for j in range(0,np.shape(N_matrix)[0]):
@@ -115,3 +128,81 @@ def u_exact(x):
     for i in range(0,len(x)):
         u[i] += Desp_function(x[i])
     return u
+
+def errorDist(u_global,Lx):
+    x = np.linspace(0,Lx,len(u_global))
+    u_e = u_exact(x)
+    error = np.zeros(len(u_global))
+    for i in range(0,len(u_global)):
+        error[i] = (abs(u_global[i]-u_e[i])/abs(u_e[i]))
+    return error,x
+
+def errorRelTol(u_global,Lx):
+    x = np.linspace(0,Lx,len(u_global))
+    u_e = u_exact(x)
+    errorAbs = (np.linalg.norm(u_global - u_e)/np.linalg.norm(u_e))
+    return errorAbs
+
+def errorRefinement(xi,Lx,NodesPerElem,typeOfElem):
+    ne = 2
+    errorAbs = 1.0
+    epsilon=[]
+    GDL = []
+    while errorAbs > 0.02:
+       N_matrix = N_nat_fun_1D(xi,NodesPerElem)
+       nodes,elements = discretize(Lx,ne,NodesPerElem)
+       x_global,u_global = interpolation(N_matrix,nodes,elements,NodesPerElem)
+       errorAbs = errorRelTol(u_global,Lx)
+       epsilon.append(errorAbs)
+       GDL.append(len(nodes)+1)
+       ne += 1
+    print("---------------------------------------------------------------------------")
+    print("Se necesitan ",ne," elementos ",typeOfElem,"con ",len(nodes)+1,"GDL para obtener un error de ",errorAbs)
+    print("---------------------------------------------------------------------------")  
+    plt.figure()
+    plt.plot(GDL,epsilon)
+    plt.axhline(y=0.02,color='r',label="error del 2%")
+    title = "Error relativo vs GDL elementos "+ typeOfElem
+    plt.title(title)
+    plt.xlabel("Número de GDL")
+    plt.ylabel("Error relativo")
+    plt.grid()
+    plt.legend()
+    return
+
+def Plot_Interpolated(x,x_global,u_global,nodes,typeOfElem):
+    plt.figure()
+    plt.plot(x,u_exact(x),label="Exacta",linestyle="--")
+    plt.plot(x_global,u_global,label = "Interpolada")
+    plt.plot(nodes,np.zeros(len(nodes)),marker="o")
+    plt.scatter(nodes,u_exact(nodes),color="r",marker="o",zorder=3,label="Desplazamiento nodal")
+    title = "Desplazamientos elementos " + typeOfElem
+    plt.title(title)
+    plt.xlabel("Posicion (m)")
+    plt.ylabel("Desplazamiento")
+    plt.legend()
+    plt.grid()
+    return
+
+def Plot_formFunc(xi,N_matrix,NodesPerElem,typeOfElem):
+    plt.figure()
+    plt.plot(xi,N_matrix[0,:],label="Funcion N1")
+    plt.plot(xi,N_matrix[1,:],label="Funcion N2")
+    if NodesPerElem ==3:
+       plt.plot(xi,N_matrix[2,:],label="Funcion N3")
+    title = "Funciones de Forma elemento "+ typeOfElem
+    plt.title(title)
+    plt.legend()
+    plt.xlabel("xi")
+    plt.grid()
+    return
+
+def PlotErrorDist(u_global,Lx):
+    error,x_e = errorDist(u_global,Lx)
+    plt.figure()
+    plt.plot(x_e,error)
+    plt.axhline(y=0.02,color='r')
+    plt.title("Distribucion de error")
+    plt.xlabel("Posicion (m)")
+    plt.ylabel("error relativo")
+    plt.grid()
--- main.py
+++ main.py
@@ -1,28 +1,29 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import functions as func
+
 #----Input Variables---#
-NodesPerElem = 3
-Lx = 1.0
-ne = 5
-xi = np.linspace(-1,1,100)
-x = np.linspace(0,Lx,100)
+N = 200 #Mesh nodes
+NodesPerElem = 2 # Number of nodes per element
+Lx = 1.0 #Total longitude
+ne = 5 #Number of elements
+xi = np.linspace(-1,1,N)
+x = np.linspace(0,Lx,N)
+
+if NodesPerElem==3:
+    typeOfElem = "Cuadraticos"
+else:
+    typeOfElem = "Lineales"
+
 N_matrix = func.N_nat_fun_1D(xi,NodesPerElem)
 nodes,elements = func.discretize(Lx,ne,NodesPerElem)
-print(elements)
-x_global,u_global = func.interpolation(N_matrix,nodes,elements)
-plt.figure()
-plt.plot(x,func.u_exact(x),label="Exacta")
-plt.plot(x_global,u_global,label = "Interpolada")
-plt.plot(nodes,np.zeros(len(nodes)),marker="o")
-plt.legend()
-#----Plots----#
-func.plot_mesh_1D(nodes,elements)
-plt.figure()
-plt.plot(xi,N_matrix[0,:],label="Funcion N1")
-plt.plot(xi,N_matrix[1,:],label="Funcion N2")
-if NodesPerElem ==3:
-    plt.plot(xi,N_matrix[2,:],label="Funcion N3")
-plt.title("Funciones de Forma")
-plt.legend()
+x_global,u_global = func.interpolation(N_matrix,nodes,elements,NodesPerElem)
+func.errorRefinement(xi,Lx,NodesPerElem,typeOfElem)
+
+#---Plots---#
+func.Plot_Interpolated(x,x_global,u_global,nodes,typeOfElem)
+#func.plot_mesh_1D(nodes,elements)
+func.Plot_formFunc(xi,N_matrix,NodesPerElem,typeOfElem)
+#func.PlotErrorDist(u_global,Lx)
 plt.show()
+
