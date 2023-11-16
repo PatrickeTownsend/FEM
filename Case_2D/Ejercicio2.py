ec3294c40550fc382aa83fe5589f4f35f07dda7b
ivancotachaves <146746180+ivancotachaves@users.noreply.github.com>
2023-11-16 15:05:35 +0100

    Add files via upload

--- Case_2D/Ejercicio2.py
+++ Case_2D/Ejercicio2.py
@@ -13,7 +13,7 @@ import plot_function_2D as fun
 Lx = 5     # Longitud en eje x [m]
 Ly = 5      # Longitud en eje y [m]
 nex = 2     # Número de elementos en el eje x
-ney = 1     # Número de elementos en el eje y
+ney = 2     # Número de elementos en el eje y
 t = 0.1     # Espesor [m]
 F = 10e6    # Carga distribuida [N/m] (+: hacia la superficie)
 
@@ -58,6 +58,86 @@ def discretize(Lx, Ly, nex, ney):
 nodes, elements = discretize(Lx, Ly, nex, ney)
 fun.plot_mesh(nodes,elements)
 
+#%% Integración => Kelem
+
+def dEta(xi:float,elem:int)->float:
+    if elem == 1:
+        deta = (1/4)*(xi-1)
+    elif elem==2:
+        deta = (-1/4)*(xi+1)
+    elif elem==3:
+        deta = (1/4)*(xi+1)
+    elif elem==4:
+        deta = (1/4)*(1-xi)
+    
+    return deta
+
+def dXi(eta:float,elem:int)->float:
+    if elem == 1:
+        dxi = (1/4)*(eta-1)
+    elif elem==2:
+        dxi = (1/4)*(1-eta)
+    elif elem==3:
+        dxi = (1/4)*(1+eta)
+    elif elem==4:
+        dxi = (1/4)*(1+eta)*(-1)
+    return dxi
+
+
+def Jacobian(eta:float,xi:float, coordinates:np.ndarray[float])->np.ndarray[float]:
+    x = coordinates[:,0]
+    y = coordinates[:,1]
+    dxdxi = np.matmul(np.array([[dXi(eta,1),dXi(eta,2),dXi(eta,3),dXi(eta,4)]]),x)
+    dydxi = np.matmul(np.array([[dXi(eta,1),dXi(eta,2),dXi(eta,3),dXi(eta,4)]]),y)
+
+    dxdeta = np.matmul(np.array([[dEta(xi,1),dEta(xi,2),dEta(xi,3),dEta(xi,4)]]),x)
+    dydeta = np.matmul(np.array([[dEta(xi,1),dEta(xi,2),dEta(xi,3),dEta(xi,4)]]),y)
+
+    J = np.array([[dxdxi[0],dydxi[0]],[dxdeta[0],dydeta[0]]])
+    
+    return J
+
+def MatrixB(eta:float,xi:float, coordinates:np.ndarray[float]):
+    J = Jacobian(eta,xi, coordinates)
+    J_inv = np.linalg.inv(J)
+    dN = np.zeros((4,2))
+    for i in range(0,4):
+       d = np.transpose(np.matmul(J_inv,np.array([[dXi(eta,i+1)],[dEta(xi,i+1)]])))
+       dN[i,:]+=d[0,:]
+    
+    B = np.array([[dN[0,0],0 , dN[1,0], 0, dN[2,0], 0, dN[3,0], 0], 
+                  [0, dN[0,1], 0, dN[1,1], 0, dN[2,1], 0, dN[3,1]],
+                  [dN[0,1], dN[0,0], dN[1,1], dN[1,0], dN[2,1], dN[2,0], dN[3,1], dN[3,0]]])
+    return B
+
+def MatrixD(E:float,nu:float)->np.ndarray[float]:
+    D = E/(1-nu**2)*np.array([[1,nu,0],[nu,1,0],[0,0,(1-nu)/2]])
+    return D
+
+def ComputeElemStiff(coord,D,GPE,t):
+    Gp = [[-1/np.sqrt(3),-1/np.sqrt(3)],[1/np.sqrt(3),-1/np.sqrt(3)],[1/np.sqrt(3),1/np.sqrt(3)],[-1/np.sqrt(3),1/np.sqrt(3)]]
+    W = 1.0
+    Kelem = np.zeros((8,8))
+    for i in range(0,GPE):
+        xi = Gp[i][0]
+        eta = Gp[i][1]
+        B = MatrixB(eta,xi,coord)
+        Bt = np.transpose(B)
+        J = Jacobian(eta,xi,coord)
+        detJ = np.linalg.det(J)
+        A1 = np.matmul(D,B)
+        A2 = np.matmul(Bt,A1)
+        Kelem += A2*detJ*t*W
+    return Kelem
+
+def testKelem(nodes,elem,D,t):
+    elem1 = elem[1,:]
+    coord = np.zeros((len(elem1),2))
+    for i in range(0,len(elem1)):
+        coord[i,:]+=nodes[elem1[i],:]
+    kelem = ComputeElemStiff(coord,D,4,t)
+    return kelem
+
 #%% Matriz de rigidez global.
 
 # Función para calcular la matriz de rigidez global. 
@@ -65,9 +145,12 @@ fun.plot_mesh(nodes,elements)
 # {INPUTS}: Kglobal, Kelem, elem.
 # {OUTPUTS}: Kglobal.
 
-Kglobal = np.zeros((12,12))
+Kglobal = np.zeros((18,18))
 Kelem = np.ones((8,8))
 elem = elements[0,:]
+E = 71e9
+nu = 0.33
+t = 0.1
 # for i in range(0,8):
 #     for j in range(0,8):
 #         Kelem[i,j] = j+1+(1+i)*10
@@ -83,12 +166,13 @@ def Assembly_global_k(Kglobal, Kelem, elem):
 
 
 def iterativeK(Kglobal,Kelem,elements):
-    for i in range(0,len(elements)):
-        elem = elements[i,:]
-        Kglobal += Assembly_global_k(Kglobal, Kelem, elem)
+    for i in range(0,np.shape(elements)[0]):
+        print(i)
+        elem = np.transpose(elements[i,:])
+        Kglobal = Assembly_global_k(Kglobal, Kelem, elem)
     return (Kglobal)
         
-SOLVE = Assembly_global_k(Kglobal, Kelem, elem)
-    
-    
-    
+SOLVE = iterativeK(Kglobal, Kelem, elements)
+
+
+test = testKelem(nodes, elements, MatrixD(E,nu), t)
